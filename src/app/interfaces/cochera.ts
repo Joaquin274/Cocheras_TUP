ae50b885c9f75b9fb55249a4ca47de2a77eee96c
Joaquin <joaquicapoulat@gmail.com>
2024-10-23 19:59:06 -0300

    Commit 1.5.3

--- src/app/interfaces/cochera.ts
+++ src/app/interfaces/cochera.ts
@@ -3,4 +3,12 @@ export interface Cochera{
   descripcion: string;
   deshabilitada: number;
   eliminada: number;
+}import { Estacionamiento } from "./Estacionamiento";
+
+export interface Cochera{
+    id: number;
+    descripcion: string;
+    deshabilitada: number;
+    eliminada: number;
+    estacionamiento: Estacionamiento | undefined;
 }
